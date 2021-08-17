6dde25dfef2e7a55b66097fa4c6dad99b53dbe34
Amy <amycheck8715@gmail.com>
2021-08-17 09:35:48 -0400

    implemented user login via useresource hook

--- src/user/Login.js
+++ src/user/Login.js
@@ -1,16 +1,43 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
+import { useResource } from 'react-request-hook';
+
 import { StateContext } from '../contexts';
 
 export default function Login () {
     const { dispatch } = useContext(StateContext)
     const [ username, setUsername ] = useState('')
+    const [ loginFailed, setLoginFailed ] = useState(false)
+    const [ password, setPassword ] = useState('')
+    
+    const [ user, login ] = useResource((username, password) => ({      // in real world we would send password via POST data via HTTPS
+        url: `/login/${encodeURI(username)}/${encodeURI(password)}`,
+        method: 'get'        
+    }))
+
+    useEffect(() => {
+        if (user && user.data) {
+            if (user.data.length > 0) {
+                setLoginFailed(false)
+                dispatch({ type: 'LOGIN', username: user.data[0].username})
+            } else {
+                setLoginFailed(true)
+            }
+        }
+        if (user && user.error) {
+            setLoginFailed(true)
+        }
+    }, [user])  // We make sure that the Effect Hook triggers whenever the user object from the Resource Hook updates
+
+    function handlePassword (evt) {
+        setPassword(evt.target.value)
+    }
 
     function handleUsername (evt) {
         setUsername(evt.target.value)
     }
 
     return (
-        <form onSubmit={e => { e.preventDefault(); dispatch({type: 'LOGIN', username }) }}>
+        <form onSubmit={e => { e.preventDefault(); login(username, password) }}>
             <label htmlFor="login-username">Username:</label>
             <input 
                 type="text"
@@ -22,6 +49,8 @@ export default function Login () {
             <label htmlFor="login-password">Password:</label>
             <input 
                 type="password"
+                value={password}
+                onChange={handlePassword}
                 name="login-password"
                 id="login-password"
             />
@@ -30,6 +59,7 @@ export default function Login () {
                 value="Login"
                 disabled={username.length === 0} //disables login button when username is empty
             />
+            {loginFailed && <span style={{ color: 'red' }}> Invalid username or password</span>}
         </form>
     )
 };
