816b6b6a74596740e366201c13c502f27d4f0e60
Amy <amycheck8715@gmail.com>
2021-08-17 09:17:14 -0400

    implemented registration via useresource hook

--- src/user/Register.js
+++ src/user/Register.js
@@ -1,4 +1,6 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
+import { useResource } from 'react-request-hook';
+
 import { StateContext } from '../contexts';
 
 export default function Register () {
@@ -6,6 +8,20 @@ export default function Register () {
     const [ username, setUsername ] = useState('')
     const [ password, setPassword ] = useState('')
     const [ passwordRepeat, setPasswordRepeat ] = useState('')
+    const [ user, register ] = useResource((username, password) =>
+    ({
+        url: '/users',
+        method: 'post',
+        data: { username, password }
+    }))
+
+    useEffect(() => {
+        if (user && user.data) {
+            dispatch({ type: 'REGISTER', username: user.data.username })
+        }
+    }, [user])
+
+
 
     function handleUsername (evt) {
         setUsername(evt.target.value)
@@ -20,7 +36,7 @@ export default function Register () {
     }
 
     return (
-        <form onSubmit={e => {e.preventDefault(); dispatch({type: 'REGISTER', username}) }}>
+        <form onSubmit={e => {e.preventDefault(); register(username, password) }}>
             <label htmlFor="register-username">Username:</label>
             <input
                 type="text"
